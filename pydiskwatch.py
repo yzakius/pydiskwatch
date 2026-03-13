deea204496b59f63cd6a5b7f120a57510066ab57
Isaac Ferreira Filho <dev@yzakius.me>
2026-03-13 01:24:52 -0300

    WIP

--- pydiskwatch.py
+++ pydiskwatch.py
@@ -2,16 +2,16 @@
 
 import os
 import subprocess
+from datetime import datetime
 
 from dotenv import load_dotenv as env
 
 env()
 
+hosts = [h for h in os.getenv("HOSTS", "").split(",") if h]
 
-hosts = os.getenv("HOSTS", "").split(",")
 
-
-def process_output(output: list) -> None:
+def process_output(output: list) -> list | None:
     """Process the output of the df -h command and return the disk usage of the root partition."""
     for line in output:
         splited_line = line.split()
@@ -20,14 +20,30 @@ def process_output(output: list) -> None:
 
 
 def get_disk_space_from_host_list(hosts):
+    """Get disk usage from a list of hosts by running df -h on each host."""
+    result = {}
+    timestamp = datetime.now().strftime("%Y-%m-%d")
     for host in hosts:
-        """Get disk usage from a list of hosts by running df -h on each host."""
         output = subprocess.check_output(
             ["ssh", host, "df -h"],
             text=True
         ).strip().split("\n")
         disk_space = process_output(output)
         print(host, disk_space)
+        result.update({host: [
+            {
+                "datetime": timestamp,
+                "usage": disk_space
+            }
+        ]})
+    return result
+
+def save_data(data):
+    # TODO: salvar em um arquivo json
+    # TODO: fazer com que o json seja atualizado
+    # TODO: colocar na doc
+    print(data)
 
 if __name__ == "__main__":
-    get_disk_space_from_host_list(hosts)
+    result = get_disk_space_from_host_list(hosts)
+    save_data(data=result)
