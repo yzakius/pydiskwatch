27ffc7d9c350c82fcac8104c494166cfa3ae5aad
Isaac Ferreira Filho <dev@yzakius.me>
2026-06-10 16:30:01 -0300

    feat(data): add save data support (#7)
    * WIP
    
    * WIP
    
    * feat(save): add save data

--- pydiskwatch.py
+++ pydiskwatch.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 
+import json
 import os
 import subprocess
+from datetime import datetime
 
 from dotenv import load_dotenv as env
 
 env()
 
+hosts = [h for h in os.getenv("HOSTS", "").split(",") if h]
 
-hosts = os.getenv("HOSTS", "").split(",")
 
-
-def process_output(output: list) -> None:
+def process_output(output: list) -> list | None:
     """Process the output of the df -h command and return the disk usage of the root partition."""
     for line in output:
         splited_line = line.split()
@@ -20,14 +21,37 @@ def process_output(output: list) -> None:
 
 
 def get_disk_space_from_host_list(hosts):
+    """Get disk usage from a list of hosts by running df -h on each host."""
+    result = {}
+    timestamp = datetime.now().strftime("%Y-%m-%d")
     for host in hosts:
-        """Get disk usage from a list of hosts by running df -h on each host."""
-        output = subprocess.check_output(
-            ["ssh", host, "df -h"],
-            text=True
-        ).strip().split("\n")
+        output = (
+            subprocess.check_output(["ssh", host, "df -h"], text=True)
+            .strip()
+            .split("\n")
+        )
         disk_space = process_output(output)
-        print(host, disk_space)
+        result.update({host: [{"datetime": timestamp, "usage": disk_space}]})
+    return result
+
+
+def save_data(data):
+    filename = datetime.now().strftime("%Y-%m-%d")
+    if os.path.exists("history_disc_usage.json"):
+        with open("history_disc_usage.json", "r") as f:
+            history = json.load(f)
+    else:
+        history = {}
+    for host_name, host_usage_data in data.items():
+        if host_name not in history:
+            history[host_name] = []
+        history[host_name].extend(host_usage_data)
+    with open("history_disc_usage.json", "w") as f:
+        json.dump(history, f, indent=2, ensure_ascii=False)
+    # TODO: Rodar e testar se acumulou, se sim:
+    # TODO: colocar na doc
+
 
 if __name__ == "__main__":
-    get_disk_space_from_host_list(hosts)
+    result = get_disk_space_from_host_list(hosts)
+    save_data(data=result)
